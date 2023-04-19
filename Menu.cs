bb7f0212a508641cfd7db864a488a303a08d1fd9
Gabe Nascimento <Gabrielgns14@gmail.com>
2023-04-18 23:10:17 -0300

    Implementação do menu de funcionario, com as opções já programadas

--- Menu.cs
+++ Menu.cs
@@ -32,6 +32,7 @@ class Menu
 
     public void Funcionario()
     {
+        Console.Clear();
         Console.WriteLine("Seja bem vindo :-)");
         Console.WriteLine("O que deseja fazer hoje? ");
         Console.WriteLine("1 - Cadastrar Novos produtos");
@@ -42,15 +43,18 @@ class Menu
         switch (resposta)
         {
             case 1:
-                Thread.Sleep(1500);Console.Clear();
+                Thread.Sleep(1500);
+                Console.Clear();
                 MenuProdutos.CadastroDeProduto();
                 break;
             case 2:
-                Thread.Sleep(1500);Console.Clear();
+                Thread.Sleep(1500);
+                Console.Clear();
                 MenuProdutos.ListaDeProdutos();
                 break;
             default:
-                Thread.Sleep(1500);Console.Clear();
+                Thread.Sleep(1500);
+                Console.Clear();
                 Console.WriteLine("Escolha errada ... Voltando para o Menu");
                 Funcionario();
                 break;
@@ -58,4 +62,57 @@ class Menu
     }
 
     public void Cliente() { }
+
+    public void VoltarAoMenu()
+    {
+        Thread.Sleep(3000);
+        Console.Clear();
+        Console.WriteLine("Você Deseja: ");
+        Console.WriteLine("1 - Voltar ao Menu Geral");
+        Console.WriteLine("2 - Voltar ao Menu do Funcionario");
+        Console.WriteLine("3 - Voltar ao Menu do Cliente");
+        Console.WriteLine("0 - Encerrar o programa");
+        Console.Write("R: ");
+        int resposta = int.Parse(Console.ReadLine());
+
+        switch (resposta)
+        {
+            case 1:
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Indo para o Menu Inicial");
+                MenuGeral();
+                Thread.Sleep(1500);
+                break;
+            case 2:
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("indo para o Menu do Funcionario");
+                Funcionario();
+                Thread.Sleep(1500);
+                break;
+            case 3:
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Indo para o Menu do Cliente");
+                Thread.Sleep(1500);
+                Cliente();
+                break;
+            case 0:
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Saindo do programa ... ");
+                Thread.Sleep(1500);
+                System.Environment.Exit(0);
+                break;
+            default:
+            Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Escolha incorreta, encerrando o programa");
+                Thread.Sleep(1500);
+               System.Environment.Exit(0);
+                break;
+
+        }
+    }
 }
